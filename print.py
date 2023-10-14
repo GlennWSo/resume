d9bbb89e2306a19fbbc4e655f137f194e3fbfac9
GlennWSo <sondellglenn@gmail.com>
2023-10-14 22:45:19 +0200

    init auto print

--- print.py
+++ print.py
@@ -1,11 +1,34 @@
 from playwright.sync_api import sync_playwright
 
-playwright = sync_playwright().start()
+from serve import Serve
 
-browser = playwright.chromium.launch()
-page = browser.new_page()
-page.goto("https://playwright.dev/")
-page.screenshot(path="example.png")
-browser.close()
 
-playwright.stop()
+def print_page(page: str):
+    playwright = sync_playwright().start()
+
+    browser = playwright.chromium.launch()
+    page = browser.new_page()
+    page.goto("https://cv.glennwso.com/")
+    margins = 0.5
+    # page.emulate_media(media="print")
+    page.pdf(
+        path="example.pdf",
+        format="A4",
+        print_background=True,
+        margin={
+            "top": f"{margins}in",
+            "bottom": f"{margins}in",
+            "left": f"{margins}in",
+            "right": f"{margins}in",
+        },
+    )
+    browser.close()
+
+    playwright.stop()
+
+
+interface = "127.0.0.1"
+port = "1111"
+url = "http://{interface}:{port}/"
+with Serve(interface, port):
+    print_page(url)
---
+++ serve.py
@@ -0,0 +1,24 @@
+import subprocess
+from time import sleep
+
+
+def serve_zola(interface="127.0.0.1", port="1111"):
+    subprocess.run(["zola", "build"])
+    process = subprocess.Popen(
+        ["zola", "serve", "--interface", interface, "--port", port]
+    )
+
+    return process
+
+
+class Serve:
+    def __init__(self, interface="127.0.0.1", port="1111"):
+        self.interface = interface
+        self.port = port
+
+    def __enter__(self):
+        self.process = serve_zola(self.interface, self.port)
+        sleep(1)
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.process.kill()
